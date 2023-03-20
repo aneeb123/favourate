d6a9eee6c54558bbe5464e655595df59a6b5d076
ANeeb <aneebpaloth74@gmail.com>
2023-03-21 00:03:08 +0530

    social app

---
+++ script.js
@@ -0,0 +1,7 @@
+// const menuItem = document.querySelectorAll(".menu-item");
+
+// menuItem.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     item.classList.add("active");
+//   });
+// });
